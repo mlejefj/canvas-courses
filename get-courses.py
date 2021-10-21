26dd35bb0226bfda8fee853a5d06038c11fd99b3
mlejefj <mlejefj@dunwoody.edu>
2021-10-20 20:15:41 -0400

    Update pagination limit

--- get-courses.py
+++ get-courses.py
@@ -2,7 +2,7 @@ import requests
 import json
 from secrets import API_KEY
 
-url = 'https://dunwoody.instructure.com/api/v1/courses'
+url = 'https://dunwoody.instructure.com/api/v1/courses?per_page=50'
 bearer = 'Bearer ' + API_KEY
 headers = {'Authorization': bearer}
 response = requests.get(url, headers=headers)
